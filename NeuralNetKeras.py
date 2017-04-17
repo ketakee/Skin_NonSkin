a6bd1995c737da89859b78fd0813df498d29ea74
Ketakee <ketakeenimavat91@gmail.com>
2017-04-17 09:38:27 +0530

    Update NeuralNetKeras.py

--- NeuralNetKeras.py
+++ NeuralNetKeras.py
@@ -5,17 +5,14 @@ import numpy
 
 dataset = numpy.loadtxt("D:\Machine Learning - Stanford\machineLearning\Skin_NonSkin.txt", delimiter="\t")
 
+#training dataset
 X = dataset[50000:,0:3]
 Y = dataset[50000:,3]
+
+#testing dataset
 XT=dataset[1:50000,0:3]
 YT=dataset[1:50000,3]
-print(XT)
-print(YT)
-print(X)
-for i in range(len(Y)):
-    if Y[i]==2:
-        Y[i]=0
-print(Y)
+
 
 # create model
 model = Sequential()
@@ -33,3 +30,7 @@ model.fit(X, Y, nb_epoch=10, batch_size=100)
 # evaluate the model
 scores = model.evaluate(XT, YT)
 print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
+
+
+# Gives 100% accuracy in 3 epochs and takes 6 minutes to run on an i7 2.6 GHz processor with 8 Gb RAM.
